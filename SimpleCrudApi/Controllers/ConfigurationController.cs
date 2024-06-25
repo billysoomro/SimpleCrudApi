40c7ec9f3f82192385d4e3e406aa8f92efbe7a7a
billysoomro <billy.soomro02@gmail.com>
2024-06-25 21:50:46 +0100

    Removing an action from the configuration controller and adding json properties to a model

--- SimpleCrudApi/Controllers/ConfigurationController.cs
+++ SimpleCrudApi/Controllers/ConfigurationController.cs
@@ -7,20 +7,12 @@ namespace SimpleCrudApi.Controllers
     [Route("api/[controller]")]
     public class ConfigurationController : ControllerBase
     {
-        [HttpPost("SimulateCpuUsage")]
-        public IActionResult SimulateCpuUsage([FromQuery] int durationInMinutes)
-        {
-            CpuStressSimulator.SimulateCpuUsage(durationInMinutes);
-
-            return Ok($"CPU stress test for {durationInMinutes} minute(s) comlete.");
-        }
-
-        [HttpPost("SimulateCpuUsageByCores")]
-        public IActionResult SimulateCpuUsageByCores([FromQuery] int durationInMinutes)
+        [HttpGet("{durationInMinutes}")]
+        public IActionResult SimulateCpuUsageByCores(int durationInMinutes)
         {
             CpuStressSimulator.SimulateCpuUsageByCores(durationInMinutes);
 
-            return Ok($"CPU stress test for {durationInMinutes} minute(s) comlete.");
+            return Ok($"CPU stress test for {durationInMinutes} minute(s) complete.");
         }
     }
 }
--- SimpleCrudApi/Models/AWSRegionAndAZInfo.cs
+++ SimpleCrudApi/Models/AWSRegionAndAZInfo.cs
@@ -1,9 +1,13 @@
-﻿namespace SimpleCrudApi.Models
+﻿using System.Text.Json.Serialization;
+
+namespace SimpleCrudApi.Models
 {
     public class AWSRegionAndAZInfo
     {
+        [JsonPropertyName("region"), JsonRequired]
         public string Region { get; set; }
 
+        [JsonPropertyName("availabilityZone"), JsonRequired]
         public string AvailabilityZone { get; set; }
     }
 }
