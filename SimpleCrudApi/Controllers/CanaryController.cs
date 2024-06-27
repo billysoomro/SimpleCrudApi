f6b909303a0d81b9bf8dd58d5911ab5c95238c94
billysoomro <billy.soomro02@gmail.com>
2024-06-27 08:22:19 +0100

    Adding canary controller

---
+++ SimpleCrudApi/Controllers/CanaryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleCrudApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CanaryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("The SimpleCrudApi is running.");
+        }
+    }
+}
